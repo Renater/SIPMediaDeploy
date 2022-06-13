76d139c40a970ec259d5e3415b2a561c76a376da
Justin Béra <me@just1not2.org>
2022-06-13 15:40:22 +0200

    ✨(terraform) deploy coturn & kamailio
    Coturn & Kamailio should be deployed with
    terraform.

---
+++ terraform/coturn.tf
@@ -0,0 +1,31 @@
+variable "coturn_name" {
+  type        = string
+  description = "The name of the Coturn instance."
+  default     = "coturn"
+}
+
+variable "coturn_image_id" {
+  type        = string
+  description = "The ID of the image of the Coturn instance."
+}
+
+variable "coturn_flavor_id" {
+  type        = string
+  description = "The ID of the flavor of the Coturn instance."
+}
+
+variable "coturn_network_id" {
+  type        = string
+  description = "The ID of the network of the Coturn instance."
+}
+
+resource "openstack_compute_instance_v2" "coturn" {
+  name      = var.coturn_name
+  image_id  = var.coturn_image_id
+  flavor_id = var.coturn_flavor_id
+
+  network {
+    uuid = var.coturn_network_id
+    name = "public"
+  }
+}
---
+++ terraform/kamailio.tf
@@ -0,0 +1,31 @@
+variable "kamailio_name" {
+  type        = string
+  description = "The name of the Kamailio instance."
+  default     = "kamailio"
+}
+
+variable "kamailio_image_id" {
+  type        = string
+  description = "The ID of the image of the Kamailio instance."
+}
+
+variable "kamailio_flavor_id" {
+  type        = string
+  description = "The ID of the flavor of the Kamailio instance."
+}
+
+variable "kamailio_network_id" {
+  type        = string
+  description = "The ID of the network of the Kamailio instance."
+}
+
+resource "openstack_compute_instance_v2" "kamailio" {
+  name      = var.kamailio_name
+  image_id  = var.kamailio_image_id
+  flavor_id = var.kamailio_flavor_id
+
+  network {
+    uuid = var.kamailio_network_id
+    name = "public"
+  }
+}
