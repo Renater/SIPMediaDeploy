d7b7cb507663f014d88c6887394b020480565dd9
Benjamin MARY <benjamin.mary@student-cs.fr>
2022-06-15 15:31:57 +0200

    ✨(terraform) Kamailio Cloud init
    Added new vars to configure SIP and Coturn
    Completed Kamailio Cloud Init script to configure Kamailio server during instance creation

--- terraform/kamailio.cloud-init.sh
+++ terraform/kamailio.cloud-init.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 
-echo ${coturn_ip} > /root/coturn-ip.txt
-# TO COMPLETE
+KAMAILIO_IP=$(hostname -I | grep -o -E "([0-9]{1,3}\.){3}[0-9]{1,3}")
+
+GW_LAUNCHER_IP=1.2.3.4 # TODO: CHANGE THIS ADDRESS WITH THE SCALER IP LATER WHEN IMPLEMENTED
+GW_LAUNCHER_PORT=6789 # TODO: CHANGE THIS PORT WITH THE SCALER PORT LATER WHEN IMPLEMENTED
+GW_LAUNCHER_API_ROUTE=/a/b/c # TODO: CHANGE THIS ROUTE WITH THE SCALER ROUTE LATER WHEN IMPLEMENTED
+
+COTURN_IP=${coturn_ip}
+COTURN_PORT=${coturn_port}
+STUN_USER=${stun_user}
+STUN_PASS=${stun_pass}
+
+SIP_SECRET=${sip_secret}
+
+sed -i -E "s/sipSrv=\"([0-9]{1,3}\.){3}[0-9]{1,3}\"/sipSrv=\"$KAMAILIO_IP\"/" /etc/kamailio/sipmediagw.cfg
+sed -i -E "s/sipSecret=\"[^\"]*\"/sipSecret=\"$SIP_SECRET\"/" /etc/kamailio/sipmediagw.cfg
+sed -i -E "s/turnConfig=\"turn:([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5};stunuser=[^;]*;stunpass=[^\"]*\"/turnConfig=\"turn:$COTURN_IP:$COTURN_PORT;stunuser=$STUN_USER;stunpass=$STUN_PASS\"/" /etc/kamailio/sipmediagw.cfg
+sed -i -E "s;launcherAPIPath=\"http://([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5}/[^\"]*\";launcherAPIPath=\"http://$GW_LAUNCHER_IP:$GW_LAUNCHER_PORT$GW_LAUNCHER_API_ROUTE\";" /etc/kamailio/sipmediagw.cfg
+
+sed -i -E "s/alias=\"[^\"]*\"/alias=\"$KAMAILIO_IP\"/" /etc/kamailio/kamailio.cfg
+
+sed -i -E "s/SIP_DOMAIN=[^\"]*/SIP_DOMAIN=$KAMAILIO_IP/" /etc/kamailio/kamctlrc
+sed -i -E "s/DBACCESSHOST=[^\"]*/DBACCESSHOST=$KAMAILIO_IP/" /etc/kamailio/kamctlrc
--- terraform/kamailio.tf
+++ terraform/kamailio.tf
@@ -19,13 +19,22 @@ variable "kamailio_network" {
   description = "The name of the network of the Kamailio instance."
 }
 
+variable "kamailio_sip_secret" {
+  type        = string
+  description = "The secret shared by the instances using the SIP protocol."
+}
+
 resource "openstack_compute_instance_v2" "kamailio" {
   name        = var.kamailio_name
   image_name  = var.kamailio_image
   flavor_name = var.kamailio_flavor
   key_pair    = var.key_pair
   user_data = templatefile("${path.module}/kamailio.cloud-init.sh", {
-    coturn_ip = openstack_compute_instance_v2.coturn.access_ip_v4
+    coturn_ip   = openstack_compute_instance_v2.coturn.access_ip_v4,
+    coturn_port = var.coturn_port,
+    stun_user   = var.coturn_stun_user,
+    stun_pass   = var.coturn_stun_pass,
+    sip_secret  = var.kamailio_sip_secret
   })
 
   network {
