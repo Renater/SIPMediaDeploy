4ac8ecd881c01fa76624f957e64f41bd56733a2c
Benjamin MARY <benjamin.mary@student-cs.fr>
2022-06-17 15:11:24 +0200

    🚀(terraform) Add scaler in Terraform
    Added the scaler module in Terraform configuration to prepare its future deployment

---
+++ terraform/scaler.tf
@@ -0,0 +1,31 @@
+variable "scaler_name" {
+  type        = string
+  description = "The name of the scaler instance."
+  default     = "scaler"
+}
+
+variable "scaler_image" {
+  type        = string
+  description = "The name of the image of the scaler instance."
+}
+
+variable "scaler_flavor" {
+  type        = string
+  description = "The name of the flavor of the scaler instance."
+}
+
+variable "scaler_network" {
+  type        = string
+  description = "The name of the network of the scaler instance."
+}
+
+resource "openstack_compute_instance_v2" "scaler" {
+  name        = var.scaler_name
+  image_name  = var.scaler_image
+  flavor_name = var.scaler_flavor
+  key_pair    = var.key_pair
+
+  network {
+    name = var.scaler_network
+  }
+}
