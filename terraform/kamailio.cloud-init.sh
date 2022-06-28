63c47c3b3d85c418252e153dc63b23e62f7c930e
Benjamin MARY <benjamin.mary@student-cs.fr>
2022-06-28 15:44:43 +0200

    ✨(terraform) Add Kamailio domain name variable
    Kamailio can now be contacted using its domain name to simplify the gateway's UX

--- terraform/kamailio.cloud-init.sh
+++ terraform/kamailio.cloud-init.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 KAMAILIO_IP=$(hostname -I | grep -o -E "([0-9]{1,3}\.){3}[0-9]{1,3}")
+KAMAILIO_DOMAIN=${kamailio_domain}
 SIP_SECRET=${sip_secret}
 
 sed -i -E "s/sipSrv=\"([0-9]{1,3}\.){3}[0-9]{1,3}\"/sipSrv=\"$KAMAILIO_IP\"/" /etc/kamailio/sipmediagw.cfg
 sed -i -E "s/sipSecret=\"[^\"]*\"/sipSecret=\"$SIP_SECRET\"/" /etc/kamailio/sipmediagw.cfg
 
-sed -i -E "s/alias=\"[^\"]*\"/alias=\"$KAMAILIO_IP\"/" /etc/kamailio/kamailio.cfg
+sed -i -E "s/alias=\"[^\"]*\"/alias=\"$KAMAILIO_DOMAIN\"/" /etc/kamailio/kamailio.cfg
 
 sed -i -E "s/SIP_DOMAIN=[^\"]*/SIP_DOMAIN=$KAMAILIO_IP/" /etc/kamailio/kamctlrc
 sed -i -E "s/DBACCESSHOST=[^\"]*/DBACCESSHOST=$KAMAILIO_IP/" /etc/kamailio/kamctlrc
