bc46b1cabad7cc0015f1bc799719a91390fb64bc
Justin Béra <me@just1not2.org>
2022-07-05 14:27:51 +0200

    🔥(terraform) remove kamailio/coturn creation
    Every kamailio & coturn instances are built with the
    scaler now so there is no need to create the instances
    and there ports before.

--- terraform/coturn.tf
+++ terraform/coturn.tf
@@ -39,44 +39,7 @@ variable "coturn_stun_pass" {
   description = "The password of the stun server of the Coturn instance."
 }
 
-resource "openstack_networking_port_v2" "coturn" {
-  name           = "coturn"
-  network_id     = openstack_networking_network_v2.internal.id
-  admin_state_up = "true"
-
-  fixed_ip {
-    subnet_id = openstack_networking_subnet_v2.internal_v4.id
-  }
-}
-
 resource "openstack_networking_floatingip_v2" "coturn" {
   pool        = var.external_network
   description = "coturn"
-}
-
-resource "openstack_networking_floatingip_associate_v2" "coturn" {
-  floating_ip = openstack_networking_floatingip_v2.coturn.address
-  port_id     = openstack_networking_port_v2.coturn.id
-}
-
-resource "openstack_compute_instance_v2" "coturn" {
-  name        = var.coturn_name
-  image_name  = var.coturn_image
-  flavor_name = var.coturn_flavor
-  key_pair    = var.key_pair
-  user_data = templatefile("${path.module}/coturn.cloud-init.sh", {
-    coturn_ip     = openstack_networking_floatingip_v2.coturn.address
-    coturn_port   = var.coturn_port,
-    coturn_domain = var.coturn_domain_name,
-    stun_user     = var.coturn_stun_user,
-    stun_pass     = var.coturn_stun_pass,
-  })
-
-  network {
-    port = openstack_networking_port_v2.coturn.id
-  }
-
-  metadata = {
-    "sipmediagw.group" = "coturn"
-  }
 }
--- terraform/kamailio.tf
+++ terraform/kamailio.tf
@@ -29,42 +29,7 @@ variable "kamailio_domain_name" {
   description = "The domain name linked to the Kamailio's IP address."
 }
 
-resource "openstack_networking_port_v2" "kamailio" {
-  name           = "kamailio"
-  network_id     = openstack_networking_network_v2.internal.id
-  admin_state_up = "true"
-
-  fixed_ip {
-    subnet_id = openstack_networking_subnet_v2.internal_v4.id
-  }
-}
-
 resource "openstack_networking_floatingip_v2" "kamailio" {
   pool        = var.external_network
   description = "kamailio"
-}
-
-resource "openstack_networking_floatingip_associate_v2" "kamailio" {
-  floating_ip = openstack_networking_floatingip_v2.kamailio.address
-  port_id     = openstack_networking_port_v2.kamailio.id
-}
-
-resource "openstack_compute_instance_v2" "kamailio" {
-  name        = var.kamailio_name
-  image_name  = var.kamailio_image
-  flavor_name = var.kamailio_flavor
-  key_pair    = var.key_pair
-  user_data = templatefile("${path.module}/kamailio.cloud-init.sh", {
-    sip_secret      = var.kamailio_sip_secret
-    kamailio_domain = var.kamailio_domain_name
-    kamailio_ip     = openstack_networking_floatingip_v2.kamailio.address
-  })
-
-  network {
-    port = openstack_networking_port_v2.kamailio.id
-  }
-
-  metadata = {
-    "sipmediagw.group" = "kamailio"
-  }
 }
